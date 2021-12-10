502925e91b39447067008ce69e6d873bbaf343df
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2021-12-10 12:40:52 +0100

    Moved functions from main to new file.

--- main.py
+++ main.py
@@ -1,22 +1,6 @@
-import random
-import secrets
 import keygen as kg
-import time
+import unsecure as us
 
-# Generate private_key using secrets library
-def usingSecrets():
-    bits = secrets.randbits(256)
-    bits_hex = hex(bits)
-    private_key = bits_hex[2:]
-    return private_key
-
-# Generate private_key using random library (using current time, not secure)
-def usingRandom():
-    bits = random.getrandbits(256)
-    bits_hex = hex(bits)
-    private_key = bits_hex[2:]
-    return private_key
-
-key = kg.KeyGen(256, 32)
-key.takeInput('AEHUFoeaBNgjSRBNgjio;SRbnjhirs brhsil gbnrsahilvnvn ;asvbhialrsbvliasrb vslaih')
-print(key.generateKey())
+# key = kg.KeyGen(256, 32)
+# key.takeInput('AEHUFoeaBNgjSRBNgjio;SRbnjhirs brhsil gbnrsahilvnvn ;asvbhialrsbvliasrb vslaih')
+# print(key.generateKey())
---
+++ unsecure.py
@@ -0,0 +1,16 @@
+import random
+import secrets
+
+# Generate private_key using secrets library
+def usingSecrets():
+    bits = secrets.randbits(256)
+    bits_hex = hex(bits)
+    private_key = bits_hex[2:]
+    return private_key
+
+# Generate private_key using random library (using current time, not secure)
+def usingRandom():
+    bits = random.getrandbits(256)
+    bits_hex = hex(bits)
+    private_key = bits_hex[2:]
+    return private_key
