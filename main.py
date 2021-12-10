9770f57d213e8320f0366c294d47e790c3cd2fbb
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2021-12-10 13:10:48 +0100

    Added length calc.

--- main.py
+++ main.py
@@ -4,16 +4,16 @@ import unsecure as us
 # Using secrets library
 skey = us.usingSecrets()
 print(skey)
-print(len(skey))
+print('Length:', len(skey))
 
 # Using random
 rkey = us.usingRandom()
 print(rkey)
-print(len(rkey))
+print('Length:', len(rkey))
 
 # Using keygen
 key = kg.KeyGen(256, 32)
 key.takeInput('AEHUFoeaBNgjSRBNgjio;SRbnjhirs brhsil gbnrsahilvnvn ;asvbhialrsbvliasrb vslaih')
 kkey = key.generateKey()
 print(kkey)
-print(len(kkey))
+print('Length:', len(kkey))
